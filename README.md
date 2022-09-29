3223ed4cdb6671e5fe70d16fe57bb35b48dc4ef4
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-09-28 23:17:55 -0300

    feat: :tada: Iniciando o projeto
    Iniciando o projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Minha API Node.JS
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors' /* Manter sempre como 2º import */
+import cors from 'cors'
+
+import { router } from './routes'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    // Se for uma instacia do tipo error
+    if (err instanceof Error) {
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    // Se for outro tipo
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+})
+
+app.listen(21079, () => console.log('online: 21079'))
