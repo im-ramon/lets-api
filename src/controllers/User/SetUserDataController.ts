ba1ffdb5dd1f8f9dd4dd775cc514cafa7de9ccc7
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-10-22 01:07:08 -0300

    refactor: :recycle: Refatoração do SetUserDataController e Service
    Refatoração do SetUserDataController e Service

--- src/controllers/User/SetUserDataController.ts
+++ src/controllers/User/SetUserDataController.ts
@@ -5,18 +5,13 @@ import { SetUserDataService } from '../../services/users/SetUserDataService'
 class SetUserDataController {
     async handle(req: Request, res: Response) {
         const user_id = req.user_id;
-        const { last_consumption, record_no_consumption, total_relapse, score, relapse_reasons, relapse_dates } = req.body
+        const { last_consumption } = req.body
 
         const setUserDataService = new SetUserDataService()
 
         const userData = await setUserDataService.execute({
             user_id,
             last_consumption,
-            record_no_consumption,
-            total_relapse,
-            score,
-            relapse_reasons,
-            relapse_dates
         })
 
         return res.json(userData)
--- src/services/users/SetUserDataService.ts
+++ src/services/users/SetUserDataService.ts
@@ -3,29 +3,30 @@ import prismaClient from '../../prisma'
 type SetUserDataProps = {
     user_id: string,
     last_consumption: string,
-    record_no_consumption: number,
-    total_relapse: number,
-    score: number,
-    relapse_reasons: string,
-    relapse_dates: string,
 }
 
 class SetUserDataService {
-    async execute({ user_id, last_consumption, record_no_consumption, total_relapse, score, relapse_reasons, relapse_dates }: SetUserDataProps) {
+    async execute({ user_id, last_consumption }: SetUserDataProps) {
 
         const userData = await prismaClient.userData.create({
             data: {
                 user_id,
                 last_consumption,
-                record_no_consumption,
-                total_relapse,
-                score,
+                record_no_consumption: 0,
+                total_relapse: 0,
+                score: 5,
                 last_score_update: last_consumption, // Configura a última atualização do score como último consumo inicial
-                relapse_reasons,
-                relapse_dates
+                relapse_reasons: '-',
+                relapse_dates: '-'
             },
             select: {
-                id: true
+                last_consumption: true,
+                last_score_update: true,
+                record_no_consumption: true,
+                relapse_dates: true,
+                relapse_reasons: true,
+                total_relapse: true,
+                score: true,
             }
         })
 
