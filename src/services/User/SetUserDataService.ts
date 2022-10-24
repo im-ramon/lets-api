7cb5c5704c7a6f5483038439fd3becc6b60c1347
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-10-24 16:16:32 -0300

    fix: :bug: Correção dos dados iniciais da coluna "relapse_dates"
    Correção dos dados iniciais da coluna "relapse_dates"

--- src/services/User/SetUserDataService.ts
+++ src/services/User/SetUserDataService.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import prismaClient from '../../prisma'
 
 type SetUserDataProps = {
@@ -13,11 +14,11 @@ class SetUserDataService {
                 user_id,
                 last_consumption,
                 record_no_consumption: 0,
-                record_no_consumption_formated: '{ anos: 0, meses: 0, dias: 0, horas: 0, minutos: 0, segundos: 0 }',
+                record_no_consumption_formated: '{"anos":0,"meses":0,"dias":0,"horas":0,"minutos":0,"segundos":0}',
                 total_relapse: 0,
                 score: 5,
                 last_score_update: last_consumption, // Configura a última atualização do score como último consumo inicial
-                relapse_dates: '[]',
+                relapse_dates: `["${moment(last_consumption).format('YYYY-MM-DD')}"]`,
             },
             select: {
                 last_consumption: true,
